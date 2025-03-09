6d3bcfc389ead581a3eab810680c9304dedf2a76
Didi5GD <DidiFon5GD@gmail.com>
2025-03-09 14:39:43 +0700

    Добавьте файлы проекта.

---
+++ Matan_LABA1/Basa.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MATAN_LABA1
+{
+    public class Class
+    {
+        // Функция
+        public float f(float x)
+        {
+            return (float)(Math.Pow(2, x) - 5 * Math.Pow(x, 2) + 10);
+        }
+        // Производная от функции
+        public float df(float x)
+        {
+            return (float)(Math.Pow(2, x) * Math.Log(2) - 10 * x);
+        }
+        public float ddf(float x)
+        {
+            return (float)(Math.Pow(2, x) * Math.Pow(Math.Log(2), 2) - 10);
+        }
+        public float phi(float x_past, float mod)
+        {
+            float lambda;
+            if (df(x_past) > 0)
+            {
+                lambda = -1.0f / mod;
+            }
+            else
+            {
+                lambda = 1.0f / mod;
+            }
+            return x_past + lambda * f(x_past);
+        }
+        public float MethodSimpleIteration(float a, float b, float epsilon)
+        {
+            float x_n=b;  
+            float x_past = a;
+            float lam=0;
+           
+            float[] ns = { Math.Abs(df(a)), Math.Abs(df(b))};
+            float m = ns.Min();
+            float M = ns.Max();
+
+            while (Math.Abs(x_n - x_past) > epsilon)
+            {
+                
+                if (df(x_past)>0)
+                {
+                     lam = -(1 / M);
+                }
+                else if (df(x_past) < 0)
+                {
+                    lam = 1 / m;
+                }
+                x_past = x_n;
+                x_n = x_past + lam * f(x_past); 
+                Console.WriteLine(x_n.ToString("F20"));
+            }
+            return (x_n);
+        }
+        /// <param name="side"> Accepts the string: "left" or "right" </param>
+        public float MethodNewton(float a, float b, string side, float epsilon)
+        {
+            float x_n; // В формулах x_n
+            float x_past; // В формулах x_n-1
+            switch (side)
+            {
+                case "left":
+                    x_past = a;
+                    x_n = x_past - (f(x_past) / df(x_past));
+                    while (Math.Abs(x_n - x_past) > epsilon)
+                    {
+                        x_past = x_n;
+                        x_n = x_past - (f(x_past) / df(x_past));
+                        Console.WriteLine(x_n.ToString("F20"));
+                    }
+                    
+                    return x_n;
+                case "right":
+
+                    x_past = b;
+                    x_n = b - (f(b) / df(b));
+                    while (Math.Abs(x_n - x_past) > epsilon)
+                    {
+                        x_past = x_n;
+                        x_n = x_past - (f(x_past) / df(x_past));
+                        Console.WriteLine(x_n.ToString("F20"));
+
+                    }
+                    return x_n;
+                default: break;
+            }
+            return 0;
+        }
+
+        public float MethodChord(float a, float b, float epsilon)
+        {
+            float x_n=b;
+            float x_past=a;
+           
+            if (ddf(b)*f(b)>=0)
+            {
+                x_past = a;
+                x_n = b;
+                
+                while (Math.Abs(x_n - x_past) > epsilon)
+                {
+                    x_past = x_n;
+                    float x_f = (f(x_past) - f(a)) / (x_past - a);
+                    x_n = x_past - (f(x_past) / x_f);
+                    Console.WriteLine(x_n.ToString("F20"));
+                }
+            }
+            else if (ddf(a) * f(a) >= 0)
+            {
+                x_past = b;
+                x_n = a;
+              
+                while (Math.Abs(x_n - x_past) > epsilon)
+                {
+                    x_past = x_n;
+                    float x_f = (f(b) - f(x_past)) / (b - x_past);
+                    x_n = x_past - (f(x_past) / x_f);
+                    Console.WriteLine(x_n.ToString("F20"));
+                }
+            }
+            return (x_n);
+        }
+    }
+}
---
+++ Matan_LABA1/Program.cs
@@ -0,0 +1,73 @@
+﻿using Matan_LABA1;
+using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATAN_LABA1
+{
+    internal static class Program
+    {
+    public static void Main()
+        {
+            var delta = 1e-45f;
+            Console.WriteLine($"Точность {delta}\n");
+            //Class a = new Class();
+            //Console.WriteLine($"Метод Ньютона [-2,1] слева направо {a.MethodNewton(-2,-1,"left", delta).ToString("F20")}");
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine($"Метод Ньютона [-2,1] справа налево {a.MethodNewton(-2, -1, "right", delta).ToString("F20")}");
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine($"Метод Ньютона [1,2] слева направо {a.MethodNewton(1, 2, "left", delta).ToString("F20")}");
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine($"Метод Ньютона [1,2] справа налево {a.MethodNewton(1, 2, "right", delta).ToString("F20")}");
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine($"Метод Ньютона [8,9] слева направо {a.MethodNewton(8, 9, "left", delta).ToString("F20")}");
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine($"Метод Ньютона [8,9] справо налево {a.MethodNewton(8, 9, "right", delta).ToString("F20")}");
+
+            //Console.WriteLine("\n+++++++++++++++++++++++++\n");
+
+            //Console.WriteLine($"Метод простых итераций [-2,1] {a.MethodSimpleIteration(-2, -1, delta).ToString("F20")} ");
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine($"Метод простых итераций [1,2] {a.MethodSimpleIteration(1, 2, delta).ToString("F20")} ");
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine($"Метод простых итераций [8,9] {a.MethodSimpleIteration(8, 9, delta).ToString("F20")} ");
+
+            //Console.WriteLine("\n+++++++++++++++++++++++++\n");
+
+            //Console.WriteLine($"Метод хорд [-2,-1] {a.MethodChord(-2, -1, delta).ToString("F20")} ");
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine($"Метод хорд [1,2] {a.MethodChord(1, 2, delta).ToString("F20")} ");
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine($"Метод хорд [8,9] {a.MethodChord(8, 9, delta).ToString("F20")} ");
+
+            Basa_three b = new Basa_three();
+            Console.WriteLine($"Метод Ньютона [91,101] слева направо {b.MethodNewton(91, 101, "left", delta).ToString("F20")}");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Метод Ньютона [91,101] справа налево {b.MethodNewton(91, 101, "right", delta).ToString("F20")}");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Метод простых итераций [91,101] {b.MethodSimpleIteration(91, 101, delta).ToString("F20")} ");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Метод хорд [91,101] {b.MethodChord(91, 101, delta).ToString("F20")} ");
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
